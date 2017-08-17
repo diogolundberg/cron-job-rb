ed7d9b7188d14b3cb52721cff164bbc0245291c5
Diogo Lundberg <diogo@lundberg.com.br>
2017-08-17 09:38:22 -0300

    Changes cron job message

--- app/controllers/cron_messages_controller.rb
+++ app/controllers/cron_messages_controller.rb
@@ -1,10 +1,13 @@
 class CronMessagesController < ApplicationController
 
   def index
-    @url = ENV["SLACK_WEBHOOK"]
-    @body = { "text": "Assess how to cron job" }
+    body = { "text": message }.to_json
     headers = {'Content-Type': "application/json"}
-    Typhoeus.post(@url, headers: headers, body: @body.to_json)
+    Typhoeus.post(ENV["SLACK_WEBHOOK"], headers: headers, body: body)
+  end
+
+  def message
+    @message ||= t('message', time: Time.now.strftime("%d/%m/%Y %H:%M"))
   end
 
 end
